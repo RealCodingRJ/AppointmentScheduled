2d136679457ed83204c5a6e1294e6d9662e24ebc
Ryan James <realcoder2023@gmail.com>
2025-03-20 17:11:32 -0400

    added full day to File

--- src/main/kotlin/AppointmentScheduled.kt
+++ src/main/kotlin/AppointmentScheduled.kt
@@ -15,13 +15,23 @@ fun main() {
 
         val date = LocalDateTime.now();
 
+        println("Enter Month: ")
+        val month = readln()
+        println("Enter Day: ")
+        val day = readln()
+        println("Enter Hour: ")
+        val hour = readln()
+        println("Enter Minute: ")
+        val minute = readln()
+
         val dateCurrent  = "$inputs was Added on: ${date.toLocalDate()}\n"
+        val appointmentTime = "Appointment Starts on $month/$day $hour:$minute\n";
 
         file.appendText(dateCurrent);
+        file.appendText(appointmentTime);
 
         num += 1;
         break;
     }
-
 
 }
