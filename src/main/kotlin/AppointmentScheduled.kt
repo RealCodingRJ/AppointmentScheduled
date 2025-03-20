9485cbeadd588a4c0ca4e3a67487a3276d00280d
Ryan James <realcoder2023@gmail.com>
2025-03-20 16:58:56 -0400

    added appointment schedules

---
+++ src/main/kotlin/AppointmentScheduled.kt
@@ -0,0 +1,27 @@
+﻿import java.io.File
+import java.time.LocalDateTime
+import java.util.*
+
+fun main() {
+
+    var num = 1;
+
+    while(true) {
+
+        println("Enter Appointment Text: ");
+        val inputs = readln()
+
+        val file = File("appointments.txt");
+
+        val date = LocalDateTime.now();
+
+        val dateCurrent  = "$inputs was Added on: ${date.toLocalDate()}\n"
+
+        file.appendText(dateCurrent);
+
+        num += 1;
+        break;
+    }
+
+
+}
